6c89b9586b5acc2e590e9c7bd5136b3b5bdcfe62
ellgrl <lzoik@mail.ru>
2025-03-11 08:55:05 +0300

    task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println(cat1 == cat2);
     }
 }
